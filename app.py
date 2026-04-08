3f6fac64b8a707f5366d392688dbefb7a30f9e04
Chris Brooks <chris@chrisbrooks.org>
2026-04-08 07:55:41 -0700

    Adding import of timezone to deal with depcrecated utcnow. Removed trailing slash from plural method.

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, request, abort, url_for
-from datetime import datetime
+from datetime import datetime, timezone
 
 app = Flask(__name__)
 
 
-@app.route("/api/activities/", methods=["GET"])
-def get_activities_slash():
+@app.route("/api/activities", methods=["GET"])
+def get_activities_plural():
     return {'activities': []}, 200
 
